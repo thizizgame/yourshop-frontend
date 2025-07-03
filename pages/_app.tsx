960446b875e605f8b8882babe90b9ea702f1508d
thizizgame <keepreshkaaa@gmail.com>
2025-07-03 20:19:58 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,3 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
