54e99bc9174a870f211f91fe3be0e45698a4c35b
thizizgame <keepreshkaaa@gmail.com>
2025-07-07 23:38:23 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import { client } from '@/lib/sanityClient';
 import { allProductsQuery } from '@/lib/queries';
 
